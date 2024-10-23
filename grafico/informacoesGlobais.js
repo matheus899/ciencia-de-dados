5e5409f15fe264d65ee02c29bdf9348959987367
matheus899 <105080795+matheus899@users.noreply.github.com>
2024-10-23 12:00:50 -0300

    f

--- grafico/informacoesGlobais.js
+++ grafico/informacoesGlobais.js
@@ -5,8 +5,11 @@ async function vizualizarInformacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem   <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente 
-    <span>${dados.total_pessoas_conectadas}</span>`
+    paragrafo.innerHTML = `Você sabia que o mundo tem   <span> ${dados.total_pessoas_mundo} bilhões </span> de pessoas e que aproximadamente 
+    <span>${dados.total_pessoas_conectadas} bilhões </span> estão coectadas em algum rede social e passam em média <span> ${horas} </span> horas e 
+    <span> ${ minutos}</span> minutos conectas.
+    <br> isso significa que aproximadamente ${porcentagemConectada}%
+    e pessoas estão conectadas em alguma rede social.`
 
 }
 
